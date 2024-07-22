c13d1f4c6f435a8549888dace11d9c3cd20de37e
Amal T S <amal.ts@gemsessence.com>
2024-07-22 20:08:35 +0530

    Added single number

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,5 +1,6 @@
 class StringCalculator
   def self.add(str_nums)
     return 0 if str_nums.empty?
+    return str_nums.to_i
   end
 end
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -4,9 +4,24 @@ require 'rspec'
 require_relative '../lib/string_calculator'
 
 RSpec.describe StringCalculator do
+  subject { StringCalculator }
   describe ".add" do
     it "returns 0 for an empty string" do
-      expect(StringCalculator.add("")).to eq(0)
+      expect(subject.add("")).to eq(0)
+    end
+
+    context "single number" do
+      it "returns 5 for 5" do
+        expect(subject.add("5")).to eq(5)
+      end
+
+      it "returns 10 for 10" do
+        expect(subject.add("10")).to eq(10)
+      end
+
+      it "returns 20 for 20" do
+        expect(subject.add("20")).to eq(20)
+      end
     end
   end
 end
